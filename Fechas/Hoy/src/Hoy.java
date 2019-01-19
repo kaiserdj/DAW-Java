6402ffd42702f04e80922734ef83549d6c34f1c9
Kaiserdj <gonferfr@gmail.com>
2019-01-19 18:06:53 +0100

    Hecho ejercicio Hoy

--- Fechas/Hoy/src/Hoy.java
+++ Fechas/Hoy/src/Hoy.java
@@ -1,9 +1,15 @@
 //Francisco Jesús González Fernández
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Hoy {
 
 	public static void main(String[] args) {
-		
-	}
+		Calendar hoy = Calendar.getInstance();
+		GregorianCalendar hoy2 = new GregorianCalendar();
 
+		System.out.println("Con Calendar: " + hoy.getTime());
+		System.out.print("Con GregorianCalendar: " + hoy2.getTime());
+	}
 }
