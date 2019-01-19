18d2ba8732609677f7fcbbe2f2160ae84bd3e1a6
Kaiserdj <gonferfr@gmail.com>
2019-01-19 19:11:28 +0100

    Hecho ejercicio PerdirCita_v1

--- Fechas/PedirCita_v1/src/PedirCita_v1.java
+++ Fechas/PedirCita_v1/src/PedirCita_v1.java
@@ -1,9 +1,33 @@
+import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 public class PedirCita_v1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		int dia, mes, año, check = 0;
+		GregorianCalendar fecha = new GregorianCalendar();
+		GregorianCalendar hoy = new GregorianCalendar();
 
+		do{
+			System.out.print("¿Dia?: ");
+			dia = teclado.nextInt();
+			System.out.print("¿Mes?: ");
+			mes = teclado.nextInt();
+			System.out.print("¿Año?: ");
+			año = teclado.nextInt();
+
+			fecha.set(año,(mes - 1),dia);
+
+			if(fecha.before(hoy)){
+				System.out.println("Error: La fecha introducida es anterior a la fecha actual.");
+			}else if(fecha.get(fecha.DAY_OF_WEEK)==1 || fecha.get(fecha.DAY_OF_WEEK)==7){
+				System.out.println("Los sábados y domingos no podemos atenderle");
+			}else{
+				System.out.println("Su cita para el día " + fecha.get(fecha.DAY_OF_MONTH) + "/" + fecha.get(fecha.MONTH) + "/" + fecha.get(fecha.YEAR) + " ha sido aceptada");
+				check++;
+			}
+		}while(check == 0);
 	}
 
 }
